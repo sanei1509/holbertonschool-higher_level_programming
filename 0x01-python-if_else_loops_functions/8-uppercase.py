251334e2db5b4cbcceb1f4bc841a646f87c355ac
Santiago Neira <santiagoneira52@gmail.com>
2022-04-26 13:46:12 -0300

    task 8 print a string transform to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for letter in str:
+        if (ord(letter) > 96 and ord(letter) <= 124):
+            letter = chr(ord(letter) - 32)
+        print(letter, end='')
+    print()
