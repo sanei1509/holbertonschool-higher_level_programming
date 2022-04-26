66ea6ff744f41c8b2d8e7081ebcd9cc600ef7e05
Santiago Neira <santiagoneira52@gmail.com>
2022-04-26 11:58:03 -0300

    using nested loops

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+for i in range(0, 10):
+    for j in range(i, 10):
+        if (i == j):
+            continue
+        elif (i == 8 and j == 9):
+            print("{:d}{:d}".format(i, j))
+        elif (i != j):
+            print("{:d}{:d}".format(i, j), end=', ')
