b9328df747f283306ce0b9d58fcf8f6b1837937a
Santiago Neira <santiagoneira52@gmail.com>
2022-04-26 15:13:41 -0300

    Fizz Buzz task, if is multiple of 3 and 5

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz():
+    for n in range(1, 101):
+        if ((n % 3) == 0 and (n % 5) == 0):
+            print("FizzBuzz ", end='')
+        elif ((n % 5) == 0):
+            print("Buzz ", end='')
+        elif ((n % 3) == 0):
+            print("Fizz ", end='')
+        else:
+            print("{:d} ".format(n), end='')
+        if (n == 100):
+            continue
+    print("")
