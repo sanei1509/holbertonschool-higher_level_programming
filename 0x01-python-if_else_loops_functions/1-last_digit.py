a078ff8cb68c872db1ef360049066307a75fd4a4
Santiago Neira <santiagoneira52@gmail.com>
2022-04-26 11:57:10 -0300

    fixing problems of pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,17 @@
 import random
 number = random.randint(-10000, 10000)
 number_string = str(number)
+ld = number_string[-1:]
 
-if (int(number_string[-1:]) > 5):
-	print("Last digit of {} is {} and is greater than 5".format(number, number_string[-1:]))
-elif (int(number_string[-1:]) < 6):
-	print("Last digit of {} is {} and is less than 6 and not 0".format(number, number_string[-1:]))
-else:
-	print("Last digit of {} is {}".format(number, number_string[-1:]))
+if (int(ld) == 0):
+    print("Last digit of {} is {} and is 0".format(number, ld))
+elif (int(ld) > 5):
+    if (number < 0):
+        print("Last digit of {} is -{} and is less than 5".format(number, ld))
+    else:
+        print("Last digit of {} is {} and is greater than 5".format(number, ld))
+elif (int(ld) < 6):
+    if (number < 0 and int(ld) != 0):
+        print("Last digit of {} is -{} and is less than 6 and not 0".format(number, ld))
+    elif (number > int(ld) != 0):
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, ld))
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 
-for num in range(0, 99):
-	if (num < 10):
- 		print(num, "= 0x{:d}".format(num))
-	if (num > 9 and num < 16):
-		print(num, "= 0x{}".format(chr(87 + num)))
-	if (num > 15 and num < 26):
-		print(num, "= 0x{:d}".format(num - 6))
-	if (num > 25 and num < 32):
-		print(num, "= 0x{}".format(chr(71 + num)))
-	if (num > 31 and num < 42):
-		print(num, "= 0x{:d}".format(num - 12))
-	if (num > 41 and num < 48):
-		print(num, "= 2x{}".format(chr(55 + num)))
-	if (num > 47 and num < 58):
-		print(num, "= 0x{:d}".format(num - 18))
-	if (num > 57 and num < 64):
-		print(num, "= 3x{}".format(chr(39 + num)))
-	if (num > 63 and num < 74):
-		print(num, "= 0x{:d}".format(num - 24))
-	if (num > 73 and num < 80):
-		print(num, "= 4x{}".format(chr(23 + num)))
-	if (num > 79 and num < 90):
-		print(num, "= 0x{:d}".format(num - 30))
-	if (num > 89 and num < 96):
-		print(num, "= 5x{}".format(chr(7 + num)))
-	if (num > 95 and num < 99):
-		print(num, "= 0x{:d}".format(num - 36))
+for n in range(0, 99):
+	if (n < 10):
+ 		print(n, "= 0x{:d}".format(n))
+	if (n > 9 and n < 16):
+		print(n, "= 0x{}".format(chr(87 + n)))
+	if (n > 15 and n < 26):
+		print(n, "= 0x{:d}".format(n - 6))
+	if (n > 25 and n < 32):
+		print(n, "= 0x{}".format(chr(71 + n)))
+	if (n > 31 and n < 42):
+		print(n, "= 0x{:d}".format(n - 12))
+	if (n > 41 and n < 48):
+		print(n, "= 2x{}".format(chr(55 + n)))
+	if (n > 47 and n < 58):
+		print(n, "= 0x{:d}".format(n - 18))
+	if (n > 57 and n < 64):
+		print(n, "= 3x{}".format(chr(39 + n)))
+	if (n > 63 and n < 74):
+		print(n, "= 0x{:d}".format(n - 24))
+	if (n > 73 and n < 80):
+		print(n, "= 4x{}".format(chr(23 + n)))
+	if (n > 79 and n < 90):
+		print(n, "= 0x{:d}".format(n - 30))
+	if (n > 89 and n < 96):
+		print(n, "= 5x{}".format(chr(7 + n)))
+	if (n > 95 and n < 99):
+		print(n, "= 0x{:d}".format(n - 36))
