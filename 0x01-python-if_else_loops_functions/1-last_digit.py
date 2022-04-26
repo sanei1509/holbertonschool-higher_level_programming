ed40428b9047ca04fce426b2596000f3c41b224d
Santiago Neira <santiagoneira52@gmail.com>
2022-04-26 13:14:37 -0300

    changing and fixing task 1 por diferenecia de value and type error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 
-if number < 0:
+if(number < 0):
     l_digit = number % (-10)
 else:
     l_digit = number % 10
 
-if (l_digit < 6):
+if (l_digit < 6 and l_digit != 0):
     print(f"Last digit of {number} is {l_digit} and is less than 6 and not 0")
 elif (l_digit > 5):
     print(f"Last digit of {number} is {l_digit} and is greater than 5")
