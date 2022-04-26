ea3ec6e47e347c1080bf9a1bcbded8eb7b8273bd
Santiago Neira <santiagoneira52@gmail.com>
2022-04-26 12:24:16 -0300

    fix problems with checker

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,11 +8,11 @@ if (int(ld) == 0):
     print("Last digit of {} is {} and is 0".format(number, ld))
 elif (int(ld) > 5):
     if (number < 0):
-        print("Last digit of {} is -{} and is less than 5".format(number, ld))
+        print(f"Last digit of {number} is -{ld} and is less than 6 and not 0")
     else:
-        print("Last digit of {} is {} and is greater than 5".format(number, ld))
+        print(f"Last digit of {number} is {ld} and is greater than 5")
 elif (int(ld) < 6):
     if (number < 0 and int(ld) != 0):
-        print("Last digit of {} is -{} and is less than 6 and not 0".format(number, ld))
+        print(f"Last digit of {number} is -{ld} and is less than 6 and not 0")
     elif (number > int(ld) != 0):
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, ld))
+        print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
