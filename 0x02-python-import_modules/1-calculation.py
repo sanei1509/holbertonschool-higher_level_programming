d67a2b48ab2cd2bed6a975f4457120df610431b3
Santiago Neira <santiagoneira52@gmail.com>
2022-04-28 09:15:19 -0300

    import basic func of a calc and print them

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+print(a, "+", b, "=", add(a, b))
+print(a, "-", b, "=", sub(a, b))
+print(a, "*", b, "=", mul(a, b))
+print(a, "/", b, "=", div(a, b))
