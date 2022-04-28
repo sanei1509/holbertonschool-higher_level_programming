31b1c54e864a7e44ff523e1d45538eb5d39ce72e
Santiago Neira <santiagoneira52@gmail.com>
2022-04-28 14:32:02 -0300

    changing operators logic para que funcionen los calculos

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,22 +4,24 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    argc = len(sys.argv)
+    argc = (len(sys.argv) - 1)
     argv = sys.argv
     op = argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
 
     if (argc != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif (op != "+" | op != "-" | op != "*" | op != "/"):
+    elif (op != "+" and op != "-" and op != "*" and op != "/"):
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
-        if op is "+":
+        if (op == '+'):
             print(a, "+", b, "=", add(a, b))
-        elif op is "-":
+        elif (op == '-'):
             print(a, "-", b, "=", sub(a, b))
-        elif op is "*":
-            print(a, "*", b, "=", mul(a, b))
-        elif op is "/":
+        elif (op == '*'):
+            print(a, '*', b, "=", mul(a, b))
+        elif (op == '/'):
             print(a, "/", b, "=", div(a, b))
