3035b8f3dbeae017510d8af7a9a04e46cc8d8ad7
Santiago Neira <4373@holbertonschool.com>
2022-04-28 15:17:20 +0000

    fix problem of case 1 argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
         print(argc, "arguments.")
     elif (argc == 1):
         print(argc, "argument:".format(sys.argv[0]))
+        print("1:", sys.argv[1])
     else:
         print(argc, "arguments:")
         for i in range(1, argc + 1):
