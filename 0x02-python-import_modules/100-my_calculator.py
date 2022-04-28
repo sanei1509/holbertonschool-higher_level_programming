9a52567a4b3d72f5133db6fabad1d7ddbf3c9e67
Santiago Neira <santiagoneira52@gmail.com>
2022-04-28 15:09:38 -0300

    trying fix the calculator, no toma el mul

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,25 +4,23 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    argc = (len(sys.argv) - 1)
+    argc = len(sys.argv) - 1
     argv = sys.argv
-    op = argv[2]
 
-    if (argc != 3):
+    if(argc != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif (op != "+" and op != "-" and op != "*" and op != "/"):
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
     else:
-        op = argv[2]
         a = int(argv[1])
         b = int(argv[3])
-        if (op == '+'):
+        if(argv[2] == "+"):
             print(a, "+", b, "=", add(a, b))
-        elif (op == '-'):
+        elif(argv[2] == "-"):
             print(a, "-", b, "=", sub(a, b))
-        elif (op == '*'):
-            print(a, '*', b, "=", mul(a, b))
-        elif (op == '/'):
+        elif(argv[2] == "*"):
+            print(a, "*", b, "=", mul(a, b))
+        elif(argv[2] == "/"):
             print(a, "/", b, "=", div(a, b))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
