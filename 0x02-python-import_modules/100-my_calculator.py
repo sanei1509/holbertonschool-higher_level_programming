d226c123bf225ce9ea3c23c191e635845bcf97ce
Santiago Neira <santiagoneira52@gmail.com>
2022-04-28 14:13:25 -0300

    trying task 6 of calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,18 +8,18 @@ if __name__ == "__main__":
     argv = sys.argv
     op = argv[2]
 
-    if argc is not 3:
+    if (argc != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif argv[2] not in operators:
-	print("Unknown operator. Available operators: +, -, * and /")
+    elif (op != "+" | op != "-" | op != "*" | op != "/"):
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
         if op is "+":
-             print(a, "+", b, "=", add(a, b))
-	elif op is "-":
-             print(a, "-", b, "=", sub(a, b))
-	elif op is "*":
-             print(a, "*", b, "=", mul(a, b))
-	elif op is "/":
-              print(a, "/", b, "=", div(a, b))
+            print(a, "+", b, "=", add(a, b))
+        elif op is "-":
+            print(a, "-", b, "=", sub(a, b))
+        elif op is "*":
+            print(a, "*", b, "=", mul(a, b))
+        elif op is "/":
+            print(a, "/", b, "=", div(a, b))
