1204dd0b677db26a869861b3adc4961b630fd072
Santiago Neira <santiagoneira52@gmail.com>
2022-04-28 10:31:22 -0300

    fix problem of indent

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
- argc = len(sys.argv) - 1
- if (argc == 0):
-  print(argc, "arguments.")
- elif (argc == 1):
-  print(argc, "argument:".format(sys.argv[0]))
- else:
-  print(argc, "arguments:")
-  for i in range(1, argc + 1):
-    print("{}: {}".format(i, sys.argv[i]))
+    argc = len(sys.argv) - 1
+    if (argc == 0):
+        print(argc, "arguments.")
+    elif (argc == 1):
+        print(argc, "argument:".format(sys.argv[0]))
+    else:
+        print(argc, "arguments:")
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, sys.argv[i]))
