cd17b85296b4957144e4446c48e3ad1bcc968986
Santiago Neira <santiagoneira52@gmail.com>
2022-04-28 10:30:25 -0300

    fix problem of spaces

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-print(a, "+", b, "=", add(a, b))
-print(a, "-", b, "=", sub(a, b))
-print(a, "*", b, "=", mul(a, b))
-print(a, "/", b, "=", div(a, b))
+    print(a, "+", b, "=", add(a, b))
+    print(a, "-", b, "=", sub(a, b))
+    print(a, "*", b, "=", mul(a, b))
+    print(a, "/", b, "=", div(a, b))
