979f447cf0e83c66f980dca0923bbf962062f096
Santiago Neira <santiagoneira52@gmail.com>
2022-05-02 16:15:26 +0000

    fix problem of type validate

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
-    for num in my_list:
-        print(f"{num:d}")
+    if(type(my_list) is list)
+        for num in my_list:
+            print(f"{num:d}")
