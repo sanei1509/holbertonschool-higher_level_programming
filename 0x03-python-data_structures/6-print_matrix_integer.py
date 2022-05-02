a64457f30e4cd6308fe4e1864ac2d91f09046270
Santiago Neira <santiagoneira52@gmail.com>
2022-05-02 16:51:22 +0000

    try task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for columna in range(len(matrix[0])):
-        for fila in range(len(matrix)):
-            print(matrix[columna][fila], end=" ")
+        for fila in range(len(matrix[columna])):
+            print(matrix[columna][fila], end="")
+            if (fila < len(matrix[columna]) - 1):
+                print(" ", end="")
         print()
