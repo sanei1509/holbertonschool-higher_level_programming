3a9115ccf427a0b10412ea124121cb523d055b69
Santiago Neira <santiagoneira52@gmail.com>
2022-05-02 16:24:02 +0000

    fix problem of empty list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    if (my_list is not None)
     list_cp = my_list.copy()
     if (idx < 0 or idx > len(my_list)):
         return my_list
