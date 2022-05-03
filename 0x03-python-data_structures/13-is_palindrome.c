78cea6c378f2298371c05b4464f96800529f3b85
Santiago Neira <santiagoneira52@gmail.com>
2022-05-02 21:56:49 -0300

    aprontando para probar mañana

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,28 +1,5 @@
 #include "lists.h"
 
-void check_even_odd_length(node *head)
-{
-	node *p;
-	p = start;
-	while(1)
-	{
-		if(p == NULL)
-		{
-			printf("Even");
-			break;
-		}
-		
-		if(p->next == NULL)
-		{
-			printf("odd");
-			break;
-		}
-		
-		p = p->next->next;
-	}
-	
-}
-
 node *reverse(node *head)   //reverse the string (second part)
 {
 	node *p , *q;
@@ -35,11 +12,7 @@ node *reverse(node *head)   //reverse the string (second part)
 	if(q == NULL)
 	return p;
 	q = reverse(q);
-	p->next->next = p;
-	p->next = NULL;
-	return q;
-}
-
+	p->next-
 
 /**
  *is_palindrom - function comprobe if a linked list is palindrome or not
@@ -49,25 +22,13 @@ node *reverse(node *head)   //reverse the string (second part)
 
 int is_palindrome(listint_t **head)
 {
-	node *p, *q, *first_head *second_head;
-	p = *haed;
-	q = *head;
+	node *aux, *aux2;
 	
-	if (p->next = NULL)
+	if (*head = NULL)
 	{
 		return (1);
 	}
 
 	/*split*/
-	while (1)
-	{
-		p = p->next->next;
-		if (p == NULL)
-		{
-			second_head = q->next->next;
-			break;
-		}
-		q = q->next;
-	}
-	q->next = NULL
+        
 }
