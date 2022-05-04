487db1c9aa50483090f6870739d48dd8ab8e2b5b
Santiago Neira <santiagoneira52@gmail.com>
2022-05-04 18:48:02 -0300

    fixing case border

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if(my_list is None):
+    if(len(my_list) == 0):
         return(0)
     numerador = 0
     denominador = 0
     for tup in my_list:
         numerador += tup[0] * tup[1]
         denominador += tup[1]
+
     return (numerador / denominador)
