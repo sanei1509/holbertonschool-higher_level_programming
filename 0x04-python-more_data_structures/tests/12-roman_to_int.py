dcf833de99400f2a6ccba3b5965849523cf01fdf
Santiago Neira <santiagoneira52@gmail.com>
2022-05-04 02:11:02 -0300

    probando subir archivos de prueba en una carpeta

---
+++ 0x04-python-more_data_structures/tests/12-roman_to_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if (roman_string is not str or is None):
+        return(0)
+    else:
+        valores = {"I": 1, "V": 5, "X": 10, "C": 100, "D": 500}
+        for i in valores:
+            print(i)
+    
