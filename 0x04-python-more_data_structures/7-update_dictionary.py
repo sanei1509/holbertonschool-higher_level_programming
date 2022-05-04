15f658c4a63acd60455e20bbcf9dd8ffb9cddf5e
Santiago Neira <santiagoneira52@gmail.com>
2022-05-04 17:22:40 +0000

    fix problem of return

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,7 +3,6 @@ def update_dictionary(a_dictionary, key, value):
     for key_exist in a_dictionary:
         if(key_exist == key):
             a_dictionary.update({key: value})
-            return(a_dictionary)
-        else:
-            a_dictionary.update({key: value})
-            return(a_dictionary)
+            return(a_dictionary)        
+    a_dictionary.update({key: value})
+    return(a_dictionary)
