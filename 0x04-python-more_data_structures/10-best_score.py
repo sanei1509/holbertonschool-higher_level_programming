241dd192f60fe696e3b337c3897d9cbdaff539bd
Santiago Neira <santiagoneira52@gmail.com>
2022-05-04 18:10:32 +0000

    Display the name of the player with best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    biggest_value = 0
+    name_of_winner = ""
+    if(a_dictionary is None):
+        return None
+    for player in a_dictionary:
+        if(a_dictionary[player] > biggest_value):
+            biggest_value = a_dictionary[player]
+            name_of_winner = player
+    return(name_of_winner)
