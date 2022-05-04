39540634e7e17a28bc3b286bef193cc8eaaa611c
Santiago Neira <santiagoneira52@gmail.com>
2022-05-04 03:10:32 -0300

    update

--- 0x04-python-more_data_structures/tests/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/tests/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     copy_mx = matrix.copy()
     for i in copy_mx:
         for j in i:
-            
-        return(copy_mx)
+            print(copy_mx[0][0])
+        return(copy_mx ** 2)
