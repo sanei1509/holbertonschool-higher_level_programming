ef7aa22747fb3bf53854816b2b0cab4049954041
Santiago Neira <santiagoneira52@gmail.com>
2022-05-04 17:18:37 +0000

    test with update function

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -5,5 +5,5 @@ def update_dictionary(a_dictionary, key, value):
             a_dictionary.update({key_exist: value})
             return(a_dictionary)
         else:
-            a_dictionary[key] = value
+            a_dictionary.update({key: value})
             return(a_dictionary)
