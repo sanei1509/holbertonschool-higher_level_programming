2ee2678aa4d75f190f7dfb5a4806a3ac58ac1e8d
Santiago Neira <4373@holbertonschool.com>
2022-05-16 15:16:43 +0000

    trying correct task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     len = 0
-    for element in my_list:
+    for i in range(x):
         try:
-            print(element, end='')
+            print(my_list[i], end='')
             len += 1
         except IndexError:
             break
