5eeb15203f32158c96dc9b698b388059451d7ec7
Santiago Neira <4373@holbertonschool.com>
2022-05-16 14:18:19 +0000

    task 0 without style errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    len = 0   
+    len = 0
     for element in my_list:
-            try:
-                print(f"{element}", end ='')
-                len += 1
-            except IndexError:
-                break
+        try:
+            print(f"{element}", end='')
+            len += 1
+        except IndexError:
+            break
     print()
     return len
