e56ece334e6b413708243bc38e5a22fa81c7421c
Santiago Neira <santiagoneira52@gmail.com>
2022-05-16 15:54:50 -0300

    try advanced task

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format())
         return True
-    except (ValueError, TypeError):
+    except Exception as err:
+        write(sys.stderr, err)
         return False
