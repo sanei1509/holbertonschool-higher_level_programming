7e17355d64a04a54c5c23735340645e72102caf6
Santiago Neira <4373@holbertonschool.com>
2022-05-16 14:10:18 +0000

    task 0 print sage list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in my_list:
-            print(i)
-    except(ValueError, TypeError):
-        print("Error de rango")
+    len = 0   
+    for element in my_list:
+            try:
+                print(f"{element}", end ='')
+                len += 1
+            except IndexError:
+                break
+    print()
+    return len
