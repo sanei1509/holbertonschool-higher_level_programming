a84f76b30f3e602dcf05a1a6e573595fbf92869e
Santiago Neira <santiagoneira52@gmail.com>
2022-05-17 11:49:03 -0300

    create a public instance that return area of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+class Squar define a square for: 2-square.py
+"""
+
+
+class Square:
+    """
+    Instance attribute (private): size
+    size: def __init__(self, new_size=0):
+    """
+    def __init__(self, new_size=0):
+        self.__size = new_size
+        if (type(self.__size) != int):
+            raise TypeError("size must be an integer")
+        if(self.__size < 0):
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size * self.__size)
