d7d3cecba7b42a395b72cb720dff5f3a1bb9e448
Santiago Neira <santiagoneira52@gmail.com>
2022-05-17 11:12:23 -0300

    raise a exceptions depend of size number

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+class Squar define a square for: 1-square.py
+"""
+
+
+class Square:
+    """
+    Instance attribute (private): size
+    size: def __init__(self, new_size=0):
+    """
+    def __init__(self, new_size=0):
+        self.__size = new_size
+        if (type(self.__size) != int):
+            raise TypeError("size must be an integer")
+        if(self.__size < 0):
+            raise ValueError("size must be >= 0")
