7510b8f9f74f7bdf942f68aed694c046ea1efc8f
Santiago Neira <santiagoneira52@gmail.com>
2022-05-17 09:40:39 -0300

    create a class with a private instance attr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module 1-square.py
+Define a square with content
+"""
+
+
+class Square():
+    """
+    class with a private attribute
+    """
+    __size = None
+
+    def __init__(self, new_size=None):
+        self.__size = True
+        self.__size = new_size
