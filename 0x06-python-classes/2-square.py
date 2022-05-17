a4e55ce462b6aa95326842b96e8ca587a96f0eba
Santiago Neira <santiagoneira52@gmail.com>
2022-05-17 13:16:45 -0300

    correct style

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,5 +13,5 @@ class Square:
         self.__size = new_size
         if (type(self.__size) != int):
             raise TypeError("size must be an integer")
-        if(self.__size < 0):
+        if (self.__size < 0):
             raise ValueError("size must be >= 0")
