2dff89f51cced4195121fd16f5d1c9a83d7c5dfa
Santiago Neira <santiagoneira52@gmail.com>
2022-05-17 09:36:43 -0300

    create a empty class with doc

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module 0-square.py
+Define a empty class of a square
+"""
+
+
+class Square():
+    """
+    empty class
+    """
+    pass
+
+    """
+    pass es una declaración especial que no hace nada
+    """
