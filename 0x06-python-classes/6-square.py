d437b1d69fb855282e46df9764c6a5c075507732
Santiago Neira <santiagoneira52@gmail.com>
2022-05-17 17:22:25 -0300

    fix case border of task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -37,6 +37,8 @@ class Square:
     @position.setter
     def position(self, value):
         self.__position = value
+        if (len(value) != 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
         if (value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         if (type(value[0]) != int or type(value[1]) != int):
