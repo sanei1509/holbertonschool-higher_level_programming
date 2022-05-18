cfaee9ee59f990701e38174ae49412f8e20e327e
Santiago Neira <santiagoneira52@gmail.com>
2022-05-18 20:10:41 -0300

    fix problem with order

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -18,24 +18,11 @@ class Rectangle:
     @width.setter - set the value
     @staticmethod - return the biggest rectangle(area)
     """
-
     def __init__(self, width=0, height=0):
         """Initialize"""
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if (type(value) != int):
-            raise TypeError("height must be an integer")
-        if (value < 0):
-            raise ValueError("height must be >= 0")
-        self.__height = value
 
     @property
     def width(self):
@@ -43,11 +30,24 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if (type(value) != int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
-        if (value < 0):
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
 
     def area(self):
         """return the rectangle area"""
