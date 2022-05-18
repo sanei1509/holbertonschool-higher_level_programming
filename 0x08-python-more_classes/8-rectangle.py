a031f9134a7dd24eec79e3cb2a60947df883f49f
Santiago Neira <santiagoneira52@gmail.com>
2022-05-18 19:04:20 -0300

    fix problems

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -5,7 +5,8 @@ Print a Rectangle
 """
 
 
-class Rectangle():
+class Rectangle:
+    """Class of rectangle"""
     number_of_instances = 0
     print_symbol = "#"
     """
@@ -31,9 +32,9 @@ class Rectangle():
     @height.setter
     def height(self, value):
         if (type(value) != int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if (value < 0):
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     @property
@@ -43,9 +44,9 @@ class Rectangle():
     @width.setter
     def width(self, value):
         if (type(value) != int):
-            raise TypeError("height must be an integer")
+            raise TypeError("width must be an integer")
         if (value < 0):
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     def area(self):
