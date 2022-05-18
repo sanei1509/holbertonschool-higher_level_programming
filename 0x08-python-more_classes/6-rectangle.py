e1bb54d5627e4204d0ce458ef916dacbabe81053
Santiago Neira <santiagoneira52@gmail.com>
2022-05-18 19:15:12 -0300

    fix problems of values order

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@ Print a Rectangle
 
 
 class Rectangle:
+    """Class of a rectangle"""
     number_of_instances = 0
     """
     Attribute (private): width
@@ -29,9 +30,9 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if (type(value) != int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if (value < 0):
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     @property
@@ -41,9 +42,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if (type(value) != int):
-            raise TypeError("height must be an integer")
+            raise TypeError("width must be an integer")
         if (value < 0):
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     def area(self):
@@ -73,6 +74,7 @@ class Rectangle:
         return f"Rectangle({self.__height}, {self.__width})"
 
     def __del__(self):
-        "print a message when delete a class"
-        Rectangle.number_of_instances -= 1
+        """print a message when delete a class"""
+        if (Rectangle.number_of_instances > 0):
+            Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
