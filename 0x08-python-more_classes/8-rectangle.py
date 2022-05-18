b405a3e049e410599972a8bc9ce8636fb037d2d2
Santiago Neira <santiagoneira52@gmail.com>
2022-05-18 20:40:23 -0300

    correct order of the methods width and height

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
     def width(self, value):
         if type(value) != int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -44,7 +44,7 @@ class Rectangle:
     def height(self, value):
         if type(value) != int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
@@ -82,11 +82,13 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if type(rect_1) is not Rectangle:
+        area1 = rect_1.area
+        area2 = rect_2.area
+        if (type(rect_1) is not Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        elif type(rect_2) is not Rectangle:
+        if (type(rect_2) is not Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        else:
+        if (area2() > area1()):
             return rect_2
+        else:
+            return rect_1
