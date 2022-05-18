7b8aa878fc69fd504c54fd6e88ad0d2fdc2239ce
Santiago Neira <santiagoneira52@gmail.com>
2022-05-18 19:01:02 -0300

    fix problems

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -19,8 +19,8 @@ class Rectangle():
 
     def __init__(self, width=0, height=0):
         """Initialize"""
-        self.height = height
-        self.width = width
+        self.__height = height
+        self.__width = width
         Rectangle.number_of_instances += 1
 
     @property
