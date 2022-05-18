a3eb325188bdde78711bea6e7af9cc39505fcbb0
Santiago Neira <santiagoneira52@gmail.com>
2022-05-18 20:12:32 -0300

    fix problem with order

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -57,7 +57,7 @@ class Rectangle:
         """return the rectangle perimeter"""
         if (self.__width == 0 or self.__height == 0):
             return 0
-        return (self.__width * 2 + self.__height * 2)
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
         """print rectangle with str function"""
