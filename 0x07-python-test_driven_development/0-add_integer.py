89572a2f654d9ddfbd348bff9fb91fc3bfd0ae20
Santiago Neira <santiagoneira52@gmail.com>
2022-05-20 18:20:58 +0000

    create function add and add test file with cases

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Task 0 - module add_integers(a, b)
+"""
+def add_integer(a, b=98):
+    """function that return a( int/float ) + b( int/float )"""
+    if a != int or a != float:
+        raise TypeError("a must be an integer")
+    if b != int or b != float:
+        raise TypeError("b must be an integer")
+    """asegurar que recibamos 2 argumentos siempre"""
+    if ()
+    return a + b
