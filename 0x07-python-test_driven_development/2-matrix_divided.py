fe0cef3ec8f00a72fd4fd42017fc0a3eef2de28c
Santiago Neira <santiagoneira52@gmail.com>
2022-05-21 10:03:52 -0300

    create the function matrix_divide

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Task 1 - matrix_divided(matrix, div):
+"""
+def matrix_divided(matrix, div):
+"""function that divide all the values of the matrix"""
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+
+print matrix(matrix_divided(matrix, 3))
+
+"""EXPECTED"""
+[[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
