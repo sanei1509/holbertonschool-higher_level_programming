eb6a4f6e3ec3161bf3286aa1c14d3d67bbde1de8
Santiago Neira <santiagoneira52@gmail.com>
2022-05-21 11:17:13 -0300

    update task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,13 +2,12 @@
 """
 Task 0 - module add_integers(a, b)
 """
+
+
 def add_integer(a, b=98):
     """function that return a( int/float ) + b( int/float )"""
-    if a != int or a != float:
+    if type(a) != int or type(a) != float:
         raise TypeError("a must be an integer")
-    if b != int or b != float:
+    if type(b) != int or type(b) != float:
         raise TypeError("b must be an integer")
-    """asegurar que recibamos 2 argumentos siempre"""
-    if a == None or b == None:
-        raise TypeError("Few or too arguments")
-    return a + b
+    return int(a) + int(b)
