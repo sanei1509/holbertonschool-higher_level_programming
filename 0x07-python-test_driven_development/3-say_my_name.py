8a3eed91779b7f00b5bb7581a8680928d623329d
Santiago Neira <santiagoneira52@gmail.com>
2022-05-21 20:37:36 -0300

    create function to task 2, print the name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    """function that print complete name"""
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print(f"{My name is {first name} {last name}")
