ddf3014494ed2104322f18cd4607d00550e88b76
Santiago Neira <4373@holbertonschool.com>
2022-05-21 18:40:22 +0000

    function matrix divided with some border cases

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,16 +3,27 @@
 Task 1 - matrix_divided(matrix, div):
 """
 def matrix_divided(matrix, div):
-"""function that divide all the values of the matrix"""
-    res = []
+    """function that divide all the values of the matrix"""
     if matrix is None:
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if len(matrix[0]) != len(matrix(1)):
+    if len(matrix[0]) != len(matrix[1]):
         raise TypeError("Each row of the matrix must have the same size")
-    if div = 0:
+    if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    for listas_valores in matrix:
-       for i in lista_valores:
-           if type(i)) != int and type(i) != float:
-               rais TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    results_matrix = []
+    for listas in matrix:
+        if type(listas) != list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        c_resList = []
+        for valor in listas:
+           if type(valor) != int and type(valor) != float:
+               raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+           c_resList.append(round(valor/div, 2))
+        results_matrix.append(c_resList)
+    return results_matrix
+           
+
+
+
+
