180d9950a4e705417ee6b584b8a550a09d620ce1
Santiago Neira <4373@holbertonschool.com>
2022-05-21 18:01:10 +0000

    correct functio of add integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,8 +6,8 @@ Task 0 - module add_integers(a, b)
 
 def add_integer(a, b=98):
     """function that return a( int/float ) + b( int/float )"""
-    if type(a) != int or type(a) != float:
+    if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
-    if type(b) != int or type(b) != float:
+    if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
