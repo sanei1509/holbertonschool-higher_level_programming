3969cfa4afa5b1b77b9405463204d04b981e06cf
Santiago Neira <santiagoneira52@gmail.com>
2022-05-21 23:21:16 -0300

    correct pycodestyle of matrix_divide

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,10 +2,13 @@
 """
 Task 1 - matrix_divided(matrix, div):
 """
+
+
 def matrix_divided(matrix, div):
     """function that divide all the values of the matrix"""
+    err_msg = "matrix must be a matrix (list of lists) of integers/floats"
     if matrix is None:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(err_msg)
     if len(matrix[0]) != len(matrix[1]):
         raise TypeError("Each row of the matrix must have the same size")
     if div == 0:
@@ -14,16 +17,11 @@ def matrix_divided(matrix, div):
     results_matrix = []
     for listas in matrix:
         if type(listas) != list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(err_msg)
         c_resList = []
         for valor in listas:
-           if type(valor) != int and type(valor) != float:
-               raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-           c_resList.append(round(valor/div, 2))
+            if type(valor) != int and type(valor) != float:
+                raise TypeError(err_msg)
+            c_resList.append(round(valor/div, 2))
         results_matrix.append(c_resList)
     return results_matrix
-           
-
-
-
-
