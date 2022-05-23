ac012bf3b1fc572a63a955275c7d22e448ad21b9
Santiago Neira <santiagoneira52@gmail.com>
2022-05-23 09:36:44 -0300

    function to indentation of a text

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-""" 
+"""
 Task 4 -> 5-text_indentation.py
 space then especific characters
 """
+
+
 def text_indentation(text):
     """add text /n when ',' or ':'"""
     if type(text) != str:
         raise TypeError("text must be a string")
-    for i in range(len(text)):
-        if (text[i] == '.' and (i != 0 and i != len(text) - 1)):
-            print("{}".format(text[i+1]), end="\n")
-            print()
-        if (text[i] == ':' and (i != 0 and i != len(text) - 1)):
-            print("{}".format(text[i+1]), end="\n")
-            print()
-        if (text[i] == '?' and (i != 0 and i != len(text) - 1)):
-            print("{}".format(text[i+1]), end="\n")
-            print()
-        else:
-            print("{}".format(text[i]), end="")
+
+    ind_text = text.replace(". ", ".\n\n")
+    ind_text2 = ind_text.replace("? ", "?\n\n")
+    last_text = ind_text2.replace(": ", ":\n\n")
+    print(last_text, end="")
