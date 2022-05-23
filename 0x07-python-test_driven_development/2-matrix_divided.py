e51d7c7efa0eb8eb94b2998c0004e984ce10c97f
Santiago Neira <santiagoneira52@gmail.com>
2022-05-23 12:15:00 -0300

    correct function for two especila cases

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,10 +7,12 @@ Task 1 - matrix_divided(matrix, div):
 def matrix_divided(matrix, div):
     """function that divide all the values of the matrix"""
     err_msg = "matrix must be a matrix (list of lists) of integers/floats"
-    if matrix is None:
+    if matrix is None or len(matrix) == 0 or len(matrix[0]) == 0:
         raise TypeError(err_msg)
     if len(matrix[0]) != len(matrix[1]):
         raise TypeError("Each row of the matrix must have the same size")
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
