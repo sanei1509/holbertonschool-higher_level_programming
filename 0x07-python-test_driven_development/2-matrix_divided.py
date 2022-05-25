b47e48f82ca76e38fb116bb7396fdd21d7c19598
Santiago Neira <4373@holbertonschool.com>
2022-05-25 21:52:12 +0000

    mofify the function

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,8 +9,9 @@ def matrix_divided(matrix, div):
     err_msg = "matrix must be a matrix (list of lists) of integers/floats"
     if matrix is None or len(matrix) == 0 or len(matrix[0]) == 0:
         raise TypeError(err_msg)
-    if len(matrix[0]) != len(matrix[1]):
+    """if len(matrix[0]) != len(matrix[1]):
         raise TypeError("Each row of the matrix must have the same size")
+    """
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
     if div == 0:
