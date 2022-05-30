48aae0b4edb189e5c3889e3f3558a23ec7bb2ba3
Santiago Neira <santiagoneira52@gmail.com>
2022-05-30 18:35:04 +0000

    asign str with square values

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Task 11 - inherit 9-rectangle, based on 10-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """
+    public instance method (def area(self):)
+    public instance method (def integer_validator(self, name, value):)
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """checkear si value es un entero"""
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+    class Rectangle inherit things of BaseGeometry:
+    - used public instance method integer_validator()
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    def width(self):
+        self.integer_validator("width", width)
+
+    def height(self):
+        self.integer_validator("height", height)
+
+    def area(self):
+        """calculate area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        return '[Rectangle] ' + str(self.__width) + '/' + str(self.__height)
+
+
+class Square(Rectangle):
+    """
+    class Square inherit of the rectangle
+    (IMPORT) si quisiera importarlo -> __import__(9_rectangle).Rectangle
+    """
+    def __init__(self, size):
+        """declaro, valido, inicializo"""
+        self.size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """reasigno __str__ con datos de square"""
+        return f"[Square] {self.size}/{self.size}"
