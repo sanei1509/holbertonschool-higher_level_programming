fa8a36cb3888b276adb24ab35bca5619d7c7ba68
Santiago Neira <4373@holbertonschool.com>
2022-05-30 23:10:32 +0000

    forgot initialize

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,9 +5,15 @@ Advanced task 100 - create a class Myint
 
 
 class MyInt(int):
-    """class with operators invertes"""
-    def __eq__(self):
+    """class with operators inverted"""
+    def __init__(self, value):
+        """inicializo"""
+        self.value = value
+
+    def __eq__(self, value):
+        """!="""
         return False
 
-    def __ne__(self):
+    def __ne__(self, value):
+        """=="""
         return True
