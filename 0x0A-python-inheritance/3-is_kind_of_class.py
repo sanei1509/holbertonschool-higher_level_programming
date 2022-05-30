a013b22c0d4a5fd4a3804af20100aa39bf1cf4f8
Santiago Neira <4373@holbertonschool.com>
2022-05-30 22:05:14 +0000

    obj is an instance of a class?

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+task 3 - obj is an instance of a class?
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    return boolean si el obj es una instancia de la clase pasada
+    """
+    return isinstance(obj, a_class)
