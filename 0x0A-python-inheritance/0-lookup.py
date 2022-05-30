10b2146323be96565b9ed2038a4839ad64a56764
Santiago Neira <4373@holbertonschool.com>
2022-05-30 21:54:38 +0000

    list attr and methods of obj

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+task 0 - function that get attr and methods of a obj
+"""
+
+
+def lookup(obj):
+    """
+    list attr and methods(obj)
+    """
+    return list(dir(obj))
