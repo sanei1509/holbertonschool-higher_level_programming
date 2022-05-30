f951ca640acb784f3be4757ab871ba22c342c073
Santiago Neira <santiagoneira52@gmail.com>
2022-05-30 14:28:19 +0000

    use a method inherit in a class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Task 1 - create class with inheritance
+"""
+
+
+class MyList(list):
+    """
+    Class inherit from list attr and method
+    def print_sorted(self):
+    """
+    def print_sorted(self):
+        """print list sorted"""
+        print(sorted(self))
