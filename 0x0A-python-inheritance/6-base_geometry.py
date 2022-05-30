bd88af1e3a6270dc03c21593fa5a298c5fdd2f17
Santiago Neira <santiagoneira52@gmail.com>
2022-05-30 15:42:44 +0000

    create a public method that raise an error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Task 6 - based on 5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """
+    public instance method (def area(self):)
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
