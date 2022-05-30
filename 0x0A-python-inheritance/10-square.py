4eedba4dc81bcb5f5668d99401b7a0df9f873a9d
Santiago Neira <4373@holbertonschool.com>
2022-05-30 22:51:54 +0000

    correct task 10 and add some comments

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,11 +5,10 @@ Task 10 - based on 9-base_geometry.py
 Rectangle = __import__("9-rectangle").Rectangle
 
 
-class Square(Rectangle)
+class Square(Rectangle):
     """
     create a class square que herede de Rectangle
-    reasignando a sus valores correspondientes
-    """
+    reasignando a sus valores correspondientes"""
     def __init__(self, size):
         """Inicialización y check con validator"""
         self.__size = self.integer_validator("size", size)
