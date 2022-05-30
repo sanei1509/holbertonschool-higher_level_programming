392976d4337a872c9084e224399432e1613fff7b
Santiago Neira <santiagoneira52@gmail.com>
2022-05-30 15:13:00 +0000

    check if object if an instance of a class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Task 2 - object isainstance of a class?
+"""
+
+
+def is_same_class(obj, a_class):
+    """checkear si es una instancia de una clase especifica"""
+    return type(obj) == a_class
