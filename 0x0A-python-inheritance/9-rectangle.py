5be14cf7eecf375f18ff018e60d4cd2f4213bdff
Santiago Neira <4373@holbertonschool.com>
2022-05-30 22:46:48 +0000

    correct task 9 and add some comments

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
 """
-Task 9 - based on 7-base_geometry.py
+Task 8 - based on 7-base_geometry.py
+instanciar (width) & (height) ya validados por nuestra function
 """
-
-
-class BaseGeometry:
-    """
-    public instance method (def area(self):)
-    public instance method (def integer_validator(self, name, value):)
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """checkear si value es un entero"""
-        if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -26,18 +12,13 @@ class Rectangle(BaseGeometry):
     - used public instance method integer_validator()
     """
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-
-    def width(self):
-        self.integer_validator("width", width)
-
-    def height(self):
-        self.integer_validator("height", height)
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
 
     def area(self):
-        """calculate area of the rectangle"""
-        return self.__width * self.__height
+        """calculate area of Rectangle (b * h)"""
+        return self.__height * self.__widht
 
     def __str__(self):
+        """print a specific string (description)"""
         return '[Rectangle] ' + str(self.__width) + '/' + str(self.__height)
