4ae0e4cf697c5e600eb0e15e0166642af9a80ff3
Santiago <santiagoneira52@gmail.com>
2022-05-31 17:11:00 +0000

    show the values passed

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+task 10
+based on "9-student.py"
+retornar solo los valores pasados en la lista
+en un DICT
+"""
+
+
+class Student:
+    """
+    -Class student
+    -intance three attributes
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        function that return the dictionaryfor JSON serialization of an object
+        None - return all the dict
+
+        """
+        if attrs is not None:
+            return {
+                key: value for key, value in
+                self.__dict__.items() if key in attrs
+                }
+        return self.__dict__
