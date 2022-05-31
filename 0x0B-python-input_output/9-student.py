db18d8f29425f9692ff03c662aad15c618903917
Santiago <santiagoneira52@gmail.com>
2022-05-31 16:26:07 +0000

    instanciation of class and instnaces

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+task 9 -
+- crear clase Student
+-  instances : first_name, last_name, age
+"""
+
+
+class Student:
+    """
+    -Class student
+    -intance three attributes
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(obj):
+        """
+        function that return the dictionaryfor JSON serialization of an object
+        """
+        return obj.__dict__
