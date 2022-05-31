33cbc81b69d66252eadcfa83255ce704b497ed44
Santiago Neira <4373@holbertonschool.com>
2022-05-31 22:51:16 +0000

    fix problem of sava load data

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,9 +14,10 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 args = list(sys.argv[1:])
 
 try:
-    load_from_json_file("add_item.json")
+    args_data = load_from_json_file("add_item.json")
 
-except FileNotFoundError:
+except Exception:
     args_data = []
 
-save_to_json_file(args_data + args, "add_item.json")
+args_data.extend(args)
+save_to_json_file(args_data, "add_item.json")
