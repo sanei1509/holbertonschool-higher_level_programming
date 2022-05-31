ff59556ce511dea1612f3c128ab2b00edb456b35
Santiago Neira <4373@holbertonschool.com>
2022-05-31 22:33:22 +0000

    testing

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -24,8 +24,6 @@ class Student:
 
         """
         if attrs is not None:
-            return {
-                key: value for key, value in
-                self.__dict__.items() if key in attrs
-                }
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
         return self.__dict__
