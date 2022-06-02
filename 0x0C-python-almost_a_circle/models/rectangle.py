5fd44456a723310a73575fc331caa3cfa06f62f2
Santiago Neira <4373@holbertonschool.com>
2022-06-02 20:04:33 +0000

    update to task 5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,10 +20,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """get width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """validate and set the value width"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -32,11 +34,13 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """get height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(height) != int:
+        """validate and set the value of height"""
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be > 0")
@@ -44,10 +48,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """get x"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """validate and set value of x"""
         if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -56,12 +62,31 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """get y"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """set the value of y"""
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """calculate the area of rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """display the rectangle"""
+        for largo in range(self.__height):
+            for ancho in range(self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(cls):
+        """representation of rectangle"""
+        return (
+            f"[Rectangle] ({cls.id}) {cls.__x}/{cls.__y} - {cls.__width}/{cls.__height}"
+            )
