c816d25d6aaefd96c6d9b61e527d93b34da21c22
Santiago Neira <4373@holbertonschool.com>
2022-06-03 18:07:38 +0000

    dict representation of Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -53,3 +53,7 @@ class Square(Rectangle):
                         self.x = value
                     if key == "y":
                         self.y = value
+
+    def to_dictionary(self):
+        """__dict__ representation of Square"""
+        return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
