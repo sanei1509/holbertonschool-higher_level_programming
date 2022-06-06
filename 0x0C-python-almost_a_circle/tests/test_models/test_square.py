981e909944e083b3804ee6ad915e76620a0412bd
Santiago Neira <4373@holbertonschool.com>
2022-06-06 15:13:17 +0000

    add file test

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Tests for a class Square (square.py)
+"""
+import unittest
+import os
+from models.base import Base
+from models.square import Square
+from models.rectangle import Rectangle
+
+def test_case(unittest.TestCase):
+    """clase para pruebas"""
+    
