38f2447f22a25cb77c62620ac03351b4e7bc5953
Santiago Neira <4373@holbertonschool.com>
2022-06-06 16:21:44 +0000

    fix case border

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,10 +25,10 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """transform object py to json string"""
-        if list_dictionaries is not None or len(list_dictionaries) != 0:
-            return json.dumps(list_dictionaries)
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return []    
         else:
-            return []
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
