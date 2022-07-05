23eb843c044bd30d239896e905368ecacf8fdd6d
Santiago <santiagoneira52@gmail.com>
2022-07-05 17:00:43 +0000

    create a second table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,2 +1,6 @@
 -- Crear una segunda tabla y hacer todo lo anterior
-CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES(1, "John", 10);
+INSERT INTO second_table VALUES(2, "Alex", 3);
+INSERT INTO second_table VALUES(3, "Bob", 14);
+INSERT INTO second_table VALUES(4, "George", 8);
