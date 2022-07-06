b82f4e5374b81e53d7c39ced79d9d2fe1af30b7b
Santiago Neira <santiagoneira52@gmail.com>
2022-07-06 17:38:10 +0000

    try task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Listar todas las ciudades de California que se puedan encontrar en 'htbtn_0d_usa'
 -- Realizar una subconsulta, traer las tablas que tienen 2 cosas iguales
-SELECT name, id FROM cities
+SELECT id, name FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California');
