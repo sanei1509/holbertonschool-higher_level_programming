25ad6d84b9f9b07d4161114246faee3e4cfdca47
Santiago Neira <santiagoneira52@gmail.com>
2022-07-06 19:22:08 +0000

    comment to task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- Importar lo mismo que en los anteriores adentro de la base de datos
 -- FILTRAR SOLO LOS QUE TENGAN VALOR NULL 
--- Juntar titulo con id del genero.
+-- Traer todos los shows que no tienen genero vinculado
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
