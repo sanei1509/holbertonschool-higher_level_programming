76daebf42b5500455ff7d394b5a74e95db5931b9
Santiago Neira <santiagoneira52@gmail.com>
2022-07-06 19:46:10 +0000

    try task 13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Misma importación que los anteriores
+-- Listar generos con con el número de vistas de cada uno
+-- Solo los titulos que tengan alguna vista
+SELECT tv_genres.name AS genre, count(tv_show_genres.show_id) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+GROUP BY genre ORDER BY number_of_shows DESC;
