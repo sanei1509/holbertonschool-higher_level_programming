72ac2facc4a8449b41a1a2f08e18ae65988678b0
Santiago Neira <santiagoneira52@gmail.com>
2022-07-06 19:29:38 +0000

    fix task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- Importar desde el link hacia la base de datos
 -- Mostrar todos los datos de la tabla izquierda aunque no tenga valor la segunda
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_shows_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
