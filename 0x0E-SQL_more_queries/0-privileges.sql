412c814cc451124660308557dfa9cad911a97cd7
Santiago Neira <santiagoneira52@gmail.com>
2022-07-06 05:15:59 +0000

    first task list the grants of users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Listar los privilegios de 'user_0d_1' y  'user_0d_2' in [localhost]
+-- GRANTS -> PERMISOS
+-- SHOW GRANTS for user_0d_1;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
