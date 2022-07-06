69e7668edecf69136805a63b174c043c2eeecdbc
Santiago Neira <santiagoneira52@gmail.com>
2022-07-06 05:48:14 +0000

    upload task 1 to show tomorrow

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Crear el usuario del servidor MySQL 'user_0d_1'
 -- con todos los permisos  &&  no debe fallar si ya existe
-
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILIEGES ON * . * TO 'user_0d_1'@'localhost';
