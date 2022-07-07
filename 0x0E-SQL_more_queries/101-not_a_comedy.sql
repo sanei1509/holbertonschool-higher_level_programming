5720c65b3178abb35d09612b2f97046d122a98d5
santiago <santiagoneira52@gmail.com>
2022-07-07 18:09:03 -0300

    task 18 advanced

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,10 @@
+-- misma database cargada de las anteriores tareas
+-- task 18 advanced
+SELECT DISTINCT tv_shows.title AS title
+FROM tv_shows
+WHERE tv_shows.title NOT IN(
+	SELECT title FROM tv_shows
+		JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+		JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+		WHERE tv_genres.name = "Comedy")
+ORDER BY title ASC;
