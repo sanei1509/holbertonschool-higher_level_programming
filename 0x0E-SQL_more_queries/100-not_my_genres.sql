85e13b01284b58829e73e7910ebedcfb8a9e7d91
santiago <santiagoneira52@gmail.com>
2022-07-07 17:17:07 -0300

    fix task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,7 +3,7 @@
 SELECT tv_genres.name AS name
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE name NOT IN(
 	SELECT FROM tv_genres
 	JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
