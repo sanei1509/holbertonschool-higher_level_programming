3e0e507b38cf66cd190aa182a84ba481c16ea045
santiago <santiagoneira52@gmail.com>
2022-07-07 17:20:20 -0300

    fix task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,6 +1,6 @@
 -- misma base de datos cargada de tareas anteriores
 -- Listar todos los generos no vinculados a "Dexter"
-SELECT tv_genres.name AS name
+SELECT DISTINCT tv_genres.name AS name
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
