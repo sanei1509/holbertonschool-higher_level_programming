18d64cabb44a472d9d8036ff547dbbefa6d47065
santiago <santiagoneira52@gmail.com>
2022-07-07 17:39:06 -0300

    fix task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,5 +7,5 @@ JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id
 WHERE name NOT IN (SELECT tv_genres.name FROM tv_genres
 			JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
 			JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id
-			WHERE tv_shows.title = "Dexter");
-ORDER BY tv_genres.name ASC;
+			WHERE tv_shows.title = "Dexter")
+ORDER BY tv_genres.name;
