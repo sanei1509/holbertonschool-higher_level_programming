24b68f4779736ef08faec228f99ea1fedf8f315f
santiago <santiagoneira52@gmail.com>
2022-07-07 17:37:16 -0300

    fix task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,12 +2,11 @@
 -- Listar todos los generos no vinculados a "Dexter"
 SELECT DISTINCT tv_genres.name AS name
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE name NOT IN(
-	SELECT tv_genres.name FROM tv_genres
-		JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-		JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-		WHERE tv_shows.title = "Dexter"
-	);
-ORDER BY tv_genres.name ASC;
+JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id
+WHERE name NOT IN (SELECT tv_genres.name
+			FROM tv_genres
+			JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+			JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id
+			WHERE tv_shows.title = "Dexter")
+ORDER BY tv_genres.name;
