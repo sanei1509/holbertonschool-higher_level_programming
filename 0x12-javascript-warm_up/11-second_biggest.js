f4b24336c4ecc3dba8b2d29d1ba25af269f9fa8c
Santiago Neira <santiagoneira52@gmail.com>
2022-08-01 17:22:09 +0000

    try task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+/* obtener el segundo valor más grande */
+const argv = process.argv;
+const ordArr = [];
+
+if (argv.length <= 3) { console.log(0); } else {
+  for (let i = 2; i < argv.length; i++) {
+    const num = parseInt(argv[i]);
+    if (isNaN(num)) { continue; }
+    ordArr.push(num);
+  }
+}
+
+if (ordArr.length > 1) {
+  ordArr.sort();
+  console.log(ordArr[ordArr.length - 2]);
+}
