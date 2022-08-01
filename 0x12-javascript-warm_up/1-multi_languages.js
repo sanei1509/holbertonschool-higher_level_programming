98677f65e6381c09ef0285eee9a593a9131c97c1
Santiago Neira <santiagoneira52@gmail.com>
2022-08-01 14:33:43 +0000

    try task one

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const lineOne = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+
+console.log(lineOne);
+console.log(secondLine);
+console.log(thirdLine);
