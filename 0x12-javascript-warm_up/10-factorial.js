3a9df02b0bc985ede3c439a7f2a7043cb65cdf19
Santiago Neira <santiagoneira52@gmail.com>
2022-08-01 16:40:46 +0000

    function recursive that calculate the factorial number

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -6,5 +6,4 @@ function factorial (num) {
   if (num <= 1 || isNaN(num)) { return 1; } else { return num * factorial(num - 1); }
 }
 
-console.log(factorial('sadasd'));
 console.log(factorial(num));
