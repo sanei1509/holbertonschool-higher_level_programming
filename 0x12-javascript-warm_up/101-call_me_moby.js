6b2c8f2a739e7e37518ed420fd7cf937b0472ea9
Santiago Neira <santiagoneira52@gmail.com>
2022-08-01 18:46:53 +0000

    try fix task 15

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* Write a function that executes x times a function */
-exports.callMeMoby = function callMeMoby (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < 3; i++) {
     theFunction();
   }
