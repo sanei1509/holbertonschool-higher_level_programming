43ead085997c850ec372d3b7ad4ce57c3fc694d5
Santiago Neira <santiagoneira52@gmail.com>
2022-08-01 15:14:52 +0000

    conver to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-arg = process.argv[2];
 
-if (isNaN(arg))
-	console.log("Not a number");
-else
-	console.log("My number:", parseInt(arg));
+let arg = process.argv[2];
+
+if (isNaN(arg)) { console.log('Not a number'); } else { console.log('My number:', parseInt(arg)); }
