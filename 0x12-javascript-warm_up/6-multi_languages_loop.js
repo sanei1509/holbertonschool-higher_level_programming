0ab8cbfd8d08cd80b541a218d87ca28f67f7f093
Santiago Neira <santiagoneira52@gmail.com>
2022-08-01 15:24:25 +0000

    try task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+/* imprimir 3 lineas, task 1 pero usando arrays and loop */
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+arr.forEach(element => { console.log(element); });
