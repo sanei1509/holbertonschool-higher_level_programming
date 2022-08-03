7a67398a5720d3f10b595b4d6b326f48d2ae2b0e
santiago neira <santiagoneira52@gmail.com>
2022-08-03 00:34:27 +0000

    try task 101 adv

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* importar un diccionario y trabajar sobre el */
+const dic = require('./101-data.js').dict;
+const newDic = {};
+for (const key in dic) {
+  if (newDic[dic[key]]) newDic[dic[key]].push(key);
+  else newDic[dic[key]] = [key];
+}
+console.log(newDic);
