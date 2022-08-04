c37b1bcabf87fbf8c5f4f8e9e052f0353f91e998
Santiago Neira <santiagoneira52@gmail.com>
2022-08-04 15:22:22 +0000

    fix injection with format

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""mostrar los valores donde la tabla coincidad con el argumnento"""
+
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    """No se ejecuta cuando es importado"""
+    user = sys.argv[1]
+    passw = sys.argv[2]
+    db = sys.argv[3]
+    state_name = sys.argv[4]
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=user,
+                       passwd=passw, db=db, charset="utf8")
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
