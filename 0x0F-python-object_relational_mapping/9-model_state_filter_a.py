e6d6818fb545e96135a84c2b786722a66bd99109
Santiago Neira <santiagoneira52@gmail.com>
2022-08-04 18:50:28 +0000

    list all objects that contain the letter a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script que imprima el 1er State de 'hbtn_0e_6_usa' """
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """no se ejecuta al importar"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+
+    session = Session(bind=engine)
+    cont = 0
+    for state in session.query(State).order_by(State.id).all():
+        # HERE: no SQL query, only objects!
+        cont += 1
+        if "a" in str(state.name):
+            print("{}: {}".format(state.id, state.name))
+        if cont == 0:
+            print()
+
+    session.close()
