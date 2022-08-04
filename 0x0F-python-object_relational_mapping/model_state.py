2790bcc63eda8f3785c286deae4971fcb6b64a68
Santiago Neira <santiagoneira52@gmail.com>
2022-08-04 17:52:44 +0000

    modeling the database columns

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""First state model"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """class State"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
