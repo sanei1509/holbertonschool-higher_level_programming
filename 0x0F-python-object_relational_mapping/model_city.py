25f56bd8af74c866bac948b9aff6fbbda6e5872f
Santiago Neira <santiagoneira52@gmail.com>
2022-08-05 01:21:39 +0000

    modeling a new table cities

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Definir una nueva clase City - similar State"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """Class city que hereda de Base"""
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = (Integer, nullable=False, ForeignKey("states.id"))
