891085b951c87d295ae8ad105172163f445fb0db
Santiago Neira <santiagoneira52@gmail.com>
2022-08-06 06:23:28 +0000

    fix task one

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,46 +1,21 @@
 #!/usr/bin/python3
 "Listar todos los estados con nombres que empiecen con N de hbtn_0e_0_usa"
-# import MySQLdb
-# import sys
-
-# if __name__ == "__main__":
-#     """No se ejecuta cuando es importado"""
-
-#     mysql_username = sys.argv[1]
-#     mysql_password = sys.argv[2]
-#     database_name = sys.argv[3]
-
-# conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-#                        passwd=mysql_password, db=database_name, charset="utf8")
-
-# cur = conn.cursor()
-# cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-# query_rows = cur.fetchall()
-# for row in query_rows:
-#     print(row)
-# cur.close()
-# conn.close()
+from sys import argv
+import MySQLdb
 
 if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
+    """No se ejecuta cuando es importado"""
+    user = argv[1]
+    password = argv[2]
+    name = argv[3]
 
-    try:
-        user = argv[1]
-        password = argv[2]
-        name = argv[3]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=user,
+                           passwd=password, db=name, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT *FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-        conn = MySQLdb.connect(host="localhost", port=3306, user=user,
-                               passwd=password, db=name,
-                               charset="utf8")
-        cur = conn.cursor()
-        cur.execute("SELECT *FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-        cur.close()
-        conn.close()
-
-    except (Exception) as errors:
-        print("There was an error")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
