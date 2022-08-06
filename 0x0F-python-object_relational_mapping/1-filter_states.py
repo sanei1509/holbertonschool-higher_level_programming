dbc37b857ddc4ec8cb71ea1ea7cd9440b0a32a28
Santiago Neira <santiagoneira52@gmail.com>
2022-08-06 06:20:11 +0000

    try fix task one

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,46 @@
 #!/usr/bin/python3
 "Listar todos los estados con nombres que empiecen con N de hbtn_0e_0_usa"
-import MySQLdb
-import sys
+# import MySQLdb
+# import sys
+
+# if __name__ == "__main__":
+#     """No se ejecuta cuando es importado"""
+
+#     mysql_username = sys.argv[1]
+#     mysql_password = sys.argv[2]
+#     database_name = sys.argv[3]
+
+# conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+#                        passwd=mysql_password, db=database_name, charset="utf8")
+
+# cur = conn.cursor()
+# cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+# query_rows = cur.fetchall()
+# for row in query_rows:
+#     print(row)
+# cur.close()
+# conn.close()
 
 if __name__ == "__main__":
-    """No se ejecuta cuando es importado"""
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    from sys import argv
+    import MySQLdb
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                       passwd=mysql_password, db=database_name, charset="utf8")
+    try:
+        user = argv[1]
+        password = argv[2]
+        name = argv[3]
 
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+        conn = MySQLdb.connect(host="localhost", port=3306, user=user,
+                               passwd=password, db=name,
+                               charset="utf8")
+        cur = conn.cursor()
+        cur.execute("SELECT *FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
+
+    except (Exception) as errors:
+        print("There was an error")
