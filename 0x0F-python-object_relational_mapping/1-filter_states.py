8abf860cb3fa3424c5f2f1b049de74e33fbaa500
Santiago Neira <santiagoneira52@gmail.com>
2022-08-06 06:13:53 +0000

    try fix task one

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,12 +5,12 @@ import sys
 
 if __name__ == "__main__":
     """No se ejecuta cuando es importado"""
-    user = sys.argv[1]
-    passw = sys.argv[2]
-    db = sys.argv[3]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=user,
-                       passwd=passw, db=db, charset="utf8")
+conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                       passwd=mysql_password, db=database_name, charset="utf8")
 
 cur = conn.cursor()
 cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
