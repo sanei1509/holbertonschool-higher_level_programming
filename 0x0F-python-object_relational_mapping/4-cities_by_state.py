7deb8a54c92bbbe578e18ca74832a1c3fc9b1131
Santiago Neira <santiagoneira52@gmail.com>
2022-08-06 07:10:19 +0000

    correct task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """Listar 'cities' de la database 'hbtn_0e_4_usa' """
-
+from sys import argv
 import MySQLdb
-import sys
+
 if __name__ == "__main__":
-    """No se ejecuta cuando es importado"""
-    user = sys.argv[1]
-    passw = sys.argv[2]
-    db = sys.argv[3]
+    """No se ejecuta al ser importado"""
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=user,
-                       passwd=passw, db=db, charset="utf8")
+    conn = MySQLdb.connect(host='localhost', port=3306, user=username,
+                           passwd=password, db=database,
+                           charset="utf8")
+    cur = conn.cursor()
 
-cur = conn.cursor()
-cur.execute("SELECT cities.id, cities.name, states.name\
-            FROM cities JOIN states ON cities.state_id = states.id")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities JOIN states ON cities.state_id = states.id")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
