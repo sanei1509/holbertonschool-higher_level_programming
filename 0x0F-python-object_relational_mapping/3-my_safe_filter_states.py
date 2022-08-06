b83bb44f4d0a18e1772688325aeba1677f20a8b4
Santiago Neira <santiagoneira52@gmail.com>
2022-08-06 07:18:24 +0000

    fix task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """Prevenir la inyection of code in the task 3"""
-
+from sys import argv
 import MySQLdb
-import sys
+
+
 if __name__ == "__main__":
-    """No se ejecuta cuando es importado"""
-    _user = argv[1]
-    _password = argv[2]
-    db_name = argv[3]
-    state = argv[4]
+    """No se ejecuta al ser importado"""
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4]
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=_user,
-                           passwd=_password, db=db_name, charset="utf8")
-
+    conn = MySQLdb.connect(host='localhost', port=3306, user=username,
+                           passwd=password, db=database,
+                           charset="utf8")
     cur = conn.cursor()
     cur.execute(f"SELECT *FROM states WHERE name = %s\
-                ORDER BY id ASC;", (stateName_search,))
+                ORDER BY id ASC;", (state_name,))
     query_rows = cur.fetchall()
 
     for row in query_rows:
