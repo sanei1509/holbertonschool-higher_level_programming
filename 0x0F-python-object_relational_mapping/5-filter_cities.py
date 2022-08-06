d9555774c7f4975b1234902bff4cc1b9ba1dac3f
Santiago Neira <santiagoneira52@gmail.com>
2022-08-06 07:13:47 +0000

    correct output task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
 """Listar todas las ciudades tomadas como argumnento"""
-
+from sys import argv
 import MySQLdb
-import sys
+
+
 if __name__ == "__main__":
-    """No se ejecuta cuando es importado"""
-    user = sys.argv[1]
-    passw = sys.argv[2]
-    db = sys.argv[3]
-state_name = sys.argv[4]
+    """No se ejecuta al ser importado"""
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4]
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=user,
-                       passwd=passw, db=db, charset="utf8")
-
-cur = conn.cursor()
-cur.execute("SELECT cities.name\
-            FROM cities JOIN states ON cities.state_id = states.id\
-            WHERE states.name = %s ORDER BY cities.id ASC", (state_name,))
-query_rows = cur.fetchall()
-text = ""
-cont = 0
-for row in query_rows:
-    cont += 1
-    if cont < len(query_rows):
-        text += row[0]+', '
-    else:
-        text += row[0]
-print(text)
-cur.close()
-conn.close()
+    conn = MySQLdb.connect(host='localhost', port=3306, user=username,
+                           passwd=password, db=database,
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.name\
+                FROM cities JOIN states ON cities.state_id = states.id\
+                WHERE states.name = %s ORDER BY cities.id ASC", (state_name,))
+    query_rows = cur.fetchall()
+    text = ""
+    cont = 0
+    for row in query_rows:
+        cont += 1
+        if cont < len(query_rows):
+            text += row[0]+', '
+        else:
+            text += row[0]
+    print(text)
+    cur.close()
+    conn.close()
