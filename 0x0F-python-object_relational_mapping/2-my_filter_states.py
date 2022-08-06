af51c3c54067a683bbd58d61d2e9063c27a7ba34
Santiago Neira <santiagoneira52@gmail.com>
2022-08-06 07:02:56 +0000

    fix task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """mostrar los valores donde la tabla coincidad con el argumnento"""
-from sys import argv
 import MySQLdb
-
+from sys import argv
 
 if __name__ == "__main__":
-    """No se ejecuta cuando es importado"""
-    state_name = str(argv[4])
-
+    """ No se ejecuta al ser importado """
+    state_name = argv[4]
     conn = MySQLdb.connect(host='localhost', port=3306,
                            user=argv[1], passwd=argv[2], db=argv[3],
                            charset="utf8")
-
     cur = conn.cursor()
     cur.execute(f"SELECT * FROM states WHERE name LIKE '{state_name}'\
                 ORDER BY states.id ASC")
