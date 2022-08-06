219c6a1465f5e81be7987b7056987397a98276d1
Santiago Neira <santiagoneira52@gmail.com>
2022-08-06 06:59:24 +0000

    fix style task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,17 +6,17 @@ import MySQLdb
 
 if __name__ == "__main__":
     """No se ejecuta cuando es importado"""
-    state_name = argv[4]
+    state_name = str(argv[4])
 
-conn = MySQLdb.connect(host='localhost', port=3306,
-                       user=argv[1], passwd=argv[2], db=argv[3],
-                       charset="utf8")
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                           user=argv[1], passwd=argv[2], db=argv[3],
+                           charset="utf8")
 
-cur = conn.cursor()
-cur.execute(f"SELECT * FROM states WHERE name LIKE '{state_name}'\
-            ORDER BY states.id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM states WHERE name LIKE '{state_name}'\
+                ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
