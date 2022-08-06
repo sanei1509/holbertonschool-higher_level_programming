0a3097e7655b42eb35ba662571dfa1807e6c140c
Santiago Neira <santiagoneira52@gmail.com>
2022-08-06 06:46:39 +0000

    fix task one

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
 "Listar todos los estados con nombres que empiecen con N de hbtn_0e_0_usa"
 
-
 if __name__ == "__main__":
-    import sys
+    """ No se ejecuta al ser importado """
     import MySQLdb
-    """No se ejecuta cuando es importado"""
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                       passwd=mysql_password, db=database_name, charset="utf8")
-
-cur = conn.cursor()
-cur.execute("SELECT *FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    from sys import argv
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                           user=argv[1], passwd=argv[2], db=argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'
+                ORDER BY states.id ASC""")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
