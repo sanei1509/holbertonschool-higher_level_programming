24da732a2f71f6a69ce0ceedbb0d12fc99707d9f
Santiago Neira <santiagoneira52@gmail.com>
2022-08-18 17:28:26 +0000

    try task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #do a post of variables with curl
-curl "$1" --data "email=test@gmail.com" --data "subject=I will always be here for PLD"
+curl -sX POST "$1" --data "email=test@gmail.com" --data "subject=I will always be here for PLD"
