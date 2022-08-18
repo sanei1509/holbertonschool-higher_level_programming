077a364a82364440380bd001e87f309c8af48622
Santiago Neira <santiagoneira52@gmail.com>
2022-08-18 17:39:58 +0000

    do a post, and display the of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#do a post of variables with curl
+#do a post of variables with curl, muestra el cuerpo de la respuesta
 curl -sX POST "$1" --data "email=test@gmail.com" --data "subject=I will always be here for PLD"
