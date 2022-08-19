b432e651b2e2838037cf9a963f6b8a6d54f0d906
Santiago Neira <santiagoneira52@gmail.com>
2022-08-19 18:49:00 +0000

    try task 8 outputs

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,22 +4,20 @@ Script que tome una letra a una url especifica
 request a http://0.0.0.0:5000/search_user
 con la letra como parametro
 """
-
 if __name__ == "__main__":
     import requests
     import sys
 
     url = "http://0.0.0.0:5000/search_user"
-    if len(sys.argv) == 2:
+    q = ""
+    if len(sys.argv) > 1:
         q = sys.argv[1]
-    else:
-        q = ""
-    data_req = requests.post(url, {"q", q})
+    data_req = requests.post(url, {"q": q})
     try:
-        data_res = data_req.json()
-        if data_res == {}:
+        data_json = data_req.json()
+        if len(data_json) == 0:
             print("No result")
         else:
-            print("[{}] {}".format(data_res.get("id"), data_res.get("name")))
+            print("[{}] {}".format(data_json["id"], data_json["name"]))
     except ValueError:
         print("Not a valid JSON")
