62039875b0bd17a754df98ef0af168fa37f3671b
Santiago Neira <santiagoneira52@gmail.com>
2022-08-19 20:26:36 +0000

    change the arguments order to correct output

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     arg2 = sys.argv[2]
 
     data = requests.get('https://api.github.com/repos/{}/{}/commits'
-                        .format(arg1, arg2))
+                        .format(arg2, arg1))
 
     json_commits = data.json()
 
-    # cont = 0
-    for commit in json_commits[:10]:
-        # if cont < 10:
-        print(commit["sha"], end=": ")
-        print(commit["commit"]["author"]["name"])
-        # cont += 1
+    cont = 0
+    for commit in json_commits:
+        if cont < 10:
+            print(commit["sha"], end=": ")
+            print(commit["commit"]["author"]["name"])
+        cont += 1
