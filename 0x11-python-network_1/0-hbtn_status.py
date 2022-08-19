d327b04940865db0d5797ebacedcdcddb6119763
Santiago Neira <santiagoneira52@gmail.com>
2022-08-19 15:35:13 +0000

    add if name

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,15 +2,16 @@
 """script para obtener -> https://intranet.hbtn.io/status
     - only with urllib
 """
-import urllib.request
+if __name__ == "__main__":
+    import urllib.request
 
-url = "https://intranet.hbtn.io/status"
-data_req = urllib.request.Request(url)
+    url = "https://intranet.hbtn.io/status"
+    data_req = urllib.request.Request(url)
 
-with urllib.request.urlopen(data_req) as response:
-    content = response.read()
-    tipo = response.code
-    print("Body response:")
-    print("\t- type: ", type(content))
-    print("\t- content: ", content)
-    print("\t- utf8 content: ", content.decode())
+    with urllib.request.urlopen(data_req) as response:
+        content = response.read()
+        tipo = response.code
+        print("Body response:")
+        print("\t- type: ", type(content))
+        print("\t- content: ", content)
+        print("\t- utf8 content: ", content.decode())
