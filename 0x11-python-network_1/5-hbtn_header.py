40f72f30c51081980dd84f0ebf7757a4543e5fd4
Santiago Neira <santiagoneira52@gmail.com>
2022-08-19 17:06:03 +0000

    get the url correctly from parameter

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,8 +4,9 @@ obtener el header de la url y extrar el valor de [X-Request-Id]
 """
 if __name__ == "__main__":
     import requests
+    import sys
 
-    url = "https://intranet.hbtn.io/status"
+    url = sys.argv[1]
     data_req = requests.get(url)
 
     print(data_req.headers["X-Request-Id"])
