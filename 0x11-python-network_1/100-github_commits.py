8e4cd09548dabac7acab8fdff0e2fcb76486bb24
Santiago Neira <santiagoneira52@gmail.com>
2022-08-19 20:14:37 +0000

    try the last advanced task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+traer 10 commits de github usando su api
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    arg1 = sys.argv[1]
+    arg2 = sys.argv[2]
+
+    data = requests.get('https://api.github.com/repos/{}/{}/commits'
+                        .format(arg1, arg2))
+
+    json_commits = data.json()
+
+    cont = 0
+    for commit in json_commits:
+        if cont < 10:
+            print(commit["sha"], end=": ")
+            print(commit["commit"]["author"]["name"])
+        cont += 1
