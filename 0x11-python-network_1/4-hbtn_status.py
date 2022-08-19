d21b7aced8ac337782cc4f3dbf1c3387d3fd2941
Santiago Neira <santiagoneira52@gmail.com>
2022-08-19 16:52:07 +0000

    using request to do a fetch

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+script que haga un fetch a https://intranet.hbtn.io/status con request
+"""
+if __name__ == "__main__":
+    import requests
+
+    url = "https://intranet.hbtn.io/status"
+    data_req = requests.get(url)
+
+    print("Body response:")
+    print("\t- type:", type(data_req))
+    print("\t- content:", data_req)
