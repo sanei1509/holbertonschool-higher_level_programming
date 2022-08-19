052ab037075c1f78bbdffd205425a61ae17220f8
Santiago Neira <santiagoneira52@gmail.com>
2022-08-19 15:53:08 +0000

    fix task 2, not execute when import

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """
-
+enviar un mail a la url pasada y mostrar el cuerpo de la respuesta
 """
 if __name__ == "__main__":
-	import urllib.request
-	import urllib.parse
-	import sys
+    import urllib.request
+    import urllib.parse
+    import sys
 
-	url = sys.argv[1]
-	email = sys.argv[2]
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-	data = urllib.parse.urlencode({"email": email})
+    data = urllib.parse.urlencode({"email": email})
 
-
-	data = data.encode()
-
-	with urllib.request.urlopen(url, data) as response:
-		print(response.read().decode())
+    data = data.encode()
+    with urllib.request.urlopen(url, data) as response:
+        print(response.read().decode())
