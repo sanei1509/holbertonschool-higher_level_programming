e833d978d6ac7c4e4364a22a755b8d37043d62e0
Santiago Neira <santiagoneira52@gmail.com>
2022-09-12 15:32:26 +0000

    count the aparitions of specific character

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/* crear un script que imprima la cantidad de peliculas con el actor 18
+- del episodio pasado por parametros
+- usando axios
+- url -> https://swapi-api.hbtn.io/api/films/:id
+*/
+const axios = require('axios').default;
+
+const url = process.argv[2];
+
+axios.get(url)
+  .then((res) => {
+    const peliculas = res.data.results;
+    let apariciones = 0;
+
+    //   ITERO SOBRE MIS PELICULAS
+    for (const arr of peliculas) {
+      const personajesArr = arr.characters;
+      //   ITERO SOBRE EL ARRAY DE PERSONAJES de la pelicula
+      for (const personaje of personajesArr) {
+        if (personaje.includes('18')) { apariciones++; }
+      }
+    }
+    console.log(apariciones);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
