acd95ad612fd5e79dbdcc36b92adffbdf3d809fb
Santiago Neira <santiagoneira52@gmail.com>
2022-09-19 21:45:23 +0000

    capture event and modify the background of header

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,7 +1,6 @@
 // Script que actulalice el color del header cuando se carga el dom (evento)
-// obtengo el header
-const header = document.querySelector('header');
 // capturamos el momento en el que el DOM esta listo
 document.addEventListener('DOMContentLoaded', function () {
-  header.style.background = '#FF0000';
+  // obtengo el header
+  document.querySelector('header').style.background = '#FF0000';
 });
