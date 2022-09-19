903e476dc2bf493c37437f4210fd95ef4957480e
Santiago Neira <santiagoneira52@gmail.com>
2022-09-19 11:57:20 +0000

    update the color of a element

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+// actualizar el color del texto children del header
+const header = document.querySelector('header');
+// obtenemos el elemento que queremos actualizar o mutar
+header.style.color = '#FF0000';
